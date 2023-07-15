a771bda0ca209ebc5416d82cfb45c5baf4e2922f
Belianchikov (pr) <belyanchikovia@gmail.com>
2023-07-15 18:11:45 +0200

    Ex 6

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+
+print(response.history[0].status_code)
+print(response.history[0].url)
+
+print(response.status_code)
+print(response.url)
