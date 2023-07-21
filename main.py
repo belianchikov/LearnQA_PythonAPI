557852614ed5a2e856c5ffef9a43b630a3a4bde9
Belianchikov (pr) <belyanchikovia@gmail.com>
2023-07-21 17:44:22 +0200

    Ex 7

--- main.py
+++ main.py
@@ -1,10 +1,69 @@
 import requests
 
-response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type")
 
-for i in range(len(response.history)):
-    print(response.history[i].status_code)
-    print(response.history[i].url)
+print("POST-request without method param:\n", response.text)
 
-print(response.status_code)
-print(response.url)
+wrong_payload = {"method": "POST123"}
+
+response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params=wrong_payload)
+
+print("Wrong HEAD-request:\n", response.text)
+
+correct_payload = {"method": "PUT"}
+
+response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", params=correct_payload)
+
+print("Correct PUT-request:\n", response.text)
+
+payloads = [{"method": "GET"}, {"method": "POST"}, {"method": "PUT"}, {"method": "DELETE"}, {"method": "HEAD"}]
+
+print("\nGET-request with params")
+for i in range(5):
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nGET-request with data")
+for i in range(5):
+    response = requests.get("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nPOST-request with params")
+for i in range(5):
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nPOST-request with data")
+for i in range(5):
+    response = requests.post("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nPUT-request with params")
+for i in range(5):
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nPUT-request with data")
+for i in range(5):
+    response = requests.put("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nDELETE-request with params")
+for i in range(5):
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nDELETE-request with data")
+for i in range(5):
+    response = requests.delete("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nHEAD-request with params")
+for i in range(5):
+    response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", params=payloads[i])
+    print(payloads[i], " ", response.text)
+
+print("\nHEAD-request with data")
+for i in range(5):
+    response = requests.head("https://playground.learnqa.ru/ajax/api/compare_query_type", data=payloads[i])
+    print(payloads[i], " ", response.text)
