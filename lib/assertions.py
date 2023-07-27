0387d65126b0b39e40420f2304ddcb89c5e807d8
Belianchikov (pr) <belyanchikovia@gmail.com>
2023-07-27 13:22:18 +0200

    Added assertions methods

--- lib/assertions.py
+++ lib/assertions.py
@@ -12,3 +12,19 @@ class Assertions:
 
         assert name in response_as_dict, f"Response JSON does not have key '{name}'"
         assert response_as_dict[name] == expected_value, error_message
+
+    @staticmethod
+    def assert_json_has_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not in JSON format. Response text is '{response.text}"
+
+        assert name in response_as_dict, f"Response JSON does not have key '{name}'"
+
+    @staticmethod
+    def assert_code_status(response: Response, expected_status_code):
+        assert expected_status_code == response.status_code, \
+            f"Unexpected status code. Expected: {expected_status_code}. Actual: {response.status_code}."
+
+
--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -27,50 +27,32 @@ class TestUserRegister(BaseCase):
             'firstName': 'Anatoly',
             'lastName': 'Rich-man'
         }
-
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 200, f"Wrong response status code {response.status_code}"
-        assert "id" in response.text, f"Unexpected response text {response.text}"
+        Assertions.assert_code_status(response, 200)
+        Assertions.assert_json_has_key(response, "id")
 
     def test_create_user_with_existing_email(self):
         email = "vinkotov@example.com"
+
         data = {
             'password': '123',
             'email': email,
             'username': 'username1',
             'firstName': 'Anatoly',
-            'lastName': 'Rich'
+            'lastName': 'Rich-man'
         }
-
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Wrong response status code {response.status_code}"
+        Assertions.assert_code_status(response, 400)
         assert response.text == f"Users with email '{email}' already exists", f"Unexpected response text {response.text}"
 
     @pytest.mark.parametrize("field", fields)
     def test_create_user_without_required_fields(self, field):
-        email = "vinkotov@example.com"
-        data = {
-            'password': '123',
-            'email': email,
-            'username': 'username1',
-            'firstName': 'Anatoly',
-            'lastName': 'Rich'
-        }
-
-        data.pop(field)
-
-        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
-
-        assert response.status_code == 400, f"Wrong response status code {response.status_code}"
-        assert response.text == f"The following required params are missed: {field}", f"Unexpected response text {response.text}"
-
-    def test_create_user_without_at_sign(self):
         base_part = "base"
         domain = "domain.com"
         random_part = datetime.now().strftime("%m%d%Y%H%M%S")
-        email = f"{base_part}{random_part}{domain}"
+        email = f"{base_part}{random_part}@{domain}"
 
         data = {
             'password': '123',
@@ -80,9 +62,31 @@ class TestUserRegister(BaseCase):
             'lastName': 'Rich-man'
         }
 
+        data.pop(field)
+
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Wrong response status code {response.status_code}"
+        Assertions.assert_code_status(response, 400)
+        assert response.text == f"The following required params are missed: {field}", \
+            f"Unexpected response text {response.text}"
+
+    def test_create_user_without_at_sign(self):
+        base_part = "base"
+        domain = "domain.com"
+        random_part = datetime.now().strftime("%m%d%Y%H%M%S")
+        email_without_at_sign = f"{base_part}{random_part}{domain}"
+
+        data = {
+            'password': '123',
+            'email': email_without_at_sign,
+            'username': 'username1',
+            'firstName': 'Anatoly',
+            'lastName': 'Rich-man'
+        }
+
+        response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
+
+        Assertions.assert_code_status(response, 400)
         assert response.text == "Invalid email format", f"Unexpected response text {response.text}"
 
     def test_create_user_with_short_username(self):
@@ -95,14 +99,17 @@ class TestUserRegister(BaseCase):
             'password': '123',
             'email': email,
             'username': 'username1',
-            'firstName': '1',
+            'firstName': 'Anatoly',
             'lastName': 'Rich-man'
         }
 
+        data["firstName"] = "1"
+
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Wrong response status code {response.status_code}"
-        assert response.text == "The value of 'firstName' field is too short", f"Unexpected response text {response.text}"
+        Assertions.assert_code_status(response, 400)
+        assert response.text == "The value of 'firstName' field is too short", \
+            f"Unexpected response text {response.text}"
 
     def test_create_user_with_long_username(self):
         base_part = "base"
@@ -114,13 +121,16 @@ class TestUserRegister(BaseCase):
             'password': '123',
             'email': email,
             'username': 'username1',
-            'firstName': 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Вдали '
-                         'от всех живут они в буквенных домах на берегу Семантика большого языкового океана. '
-                         'Маленький ручеек Даль журчит по всей стране и обеспечивает ее всеми необходи',
+            'firstName': 'Anatoly',
             'lastName': 'Rich-man'
         }
 
+        data["firstName"] = "Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные " \
+                                 "тексты. Вдали от всех живут они в буквенных домах на берегу Семантика большого " \
+                                 "языкового океана. Маленький ручеек Даль журчит по всей стране и обеспечивает ее " \
+                                 "всеми необходи"
+
         response = requests.post("https://playground.learnqa.ru/api/user/", data=data)
 
-        assert response.status_code == 400, f"Wrong response status code {response.status_code}"
+        Assertions.assert_code_status(response, 400)
         assert response.text == "The value of 'firstName' field is too long", f"Unexpected response text {response.text}"
